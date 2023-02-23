bdde6e59b836b515e3481a01ccd46f7a60cda2ee
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-02-22 18:29:45 -0800

    install tailwind and configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // this is the path to the root of the app
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  }
 })
