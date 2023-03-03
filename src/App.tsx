f0140fe3cd4d7dad36199a03e1b2474ebb6a4cda
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-02 20:44:07 -0800

    creating navbar menu and setting typescript rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "@/shared/types";
+
 
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   return (
-    <div className="App">
-     <h1>App</h1>
+    <div className="App bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
